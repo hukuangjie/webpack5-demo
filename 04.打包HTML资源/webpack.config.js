cb3b80fbc1d4109edc3ac6d3c471e8bfdac2826c
胡旷杰 <754432607@qq.com>
2020-09-25 11:02:30 +0800

    webpack day2

--- 04.打包HTML资源/webpack.config.js
+++ 04.打包HTML资源/webpack.config.js
@@ -1,19 +1,18 @@
-const {
-    resolve
-} = require("path");
-
 /*
  * @Author: your name
  * @Date: 2020-09-24 23:07:22
- * @LastEditTime: 2020-09-24 23:41:21
+ * @LastEditTime: 2020-09-25 10:09:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webpack5_demo\04.打包HTML资源\webpack.config.js
  */
+
+
+//plugin 1.下载 2.引入 3.使用
 const {
     resolve
 } = require('path')
-// const HtmlWebpackPlugin = 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -25,6 +24,8 @@ module.exports = {
 
         ],
     },
-    plugins: [],
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
     mode: 'development'
 }
