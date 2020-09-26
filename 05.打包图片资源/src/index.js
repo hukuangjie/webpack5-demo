54c371636968ee3b016ae55a95d13c5b919d2e98
胡旷杰 <754432607@qq.com>
2020-09-26 20:37:37 +0800

    打包图片资源

---
+++ 05.打包图片资源/src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-26 19:08:30
+ * @LastEditTime: 2020-09-26 19:08:44
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack5_demo\05.打包图片资源\src\index.js
+ */
+import './index.less'
---
+++ 05.打包图片资源/webpack.config.js
@@ -0,0 +1,47 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-26 19:09:07
+ * @LastEditTime: 2020-09-26 20:34:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack5_demo\05.打包图片资源\webpack.config.js
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {
+    resolve
+} = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname + '/build')
+    },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'less-loader',
+            ],
+        }, {
+            test: /\.(jpg|png|gif)$/,
+            loader: 'url-loader',
+            options: {
+                limit: 8 * 1024,
+                esModule:false,
+                name:'[hash:10].[ext]'
+            },
+        }, {
+            test: /\.html$/,
+            // 处理html文件的img图片
+            loader: 'html-loader',
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    mode: "development"
+}
