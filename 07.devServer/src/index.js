44fd8df78373faad46b0583a5232d0c46db5e902
胡旷杰 <754432607@qq.com>
2020-09-26 21:26:12 +0800

    devServer学习

---
+++ 07.devServer/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-26 20:52:31
+ * @LastEditTime: 2020-09-26 21:10:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack5_demo\06.打包其他资源\src\index.js
+ */
+import './iconfont.css'
+
+function add(x, y) {
+    return x + y
+
+}
+
+add(1, 2)
---
+++ 07.devServer/webpack.config.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-26 20:46:14
+ * @LastEditTime: 2020-09-26 21:24:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack5_demo\06.打包其他资源\webpack.config.js
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {
+    resolve
+} = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            // 打包其他资源 (除了html,js.css 以为的资源)
+            exclude: /\.(css|js|html|less)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[hash:10].[ext]'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    mode: "development",
+
+    // 开发服务器 devServer,用来自动化
+    devServer: {
+        contentBase: resolve(__dirname, 'build'),
+        // 启动gzip永硕
+        compress: true,
+        port: 3000,
+        open: true, //自动打开浏览器
+    }
+}
